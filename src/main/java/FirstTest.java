bd476091437de54f2ebdaa4143b60d8eafd5ccff
KrystynaCal <KrystynaCal5@gmail.com>
2022-08-04 15:06:42 +0200

    Initialization of driver (Chrome)

---
+++ src/main/java/FirstTest.java
@@ -0,0 +1,11 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class FirstTest {
+    @Test
+    public void demoTest() {
+        System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+    }
+}
