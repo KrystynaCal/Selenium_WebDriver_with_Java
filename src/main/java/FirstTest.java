ce391e8d5d0d97adcaece70fcd57e1d2a2f5b986
KrystynaCal <KrystynaCal5@gmail.com>
2022-08-06 17:14:51 +0200

    JavascriptExecutor

--- src/main/java/FirstTest.java
+++ src/main/java/FirstTest.java
@@ -1,8 +1,7 @@
 import org.asynchttpclient.util.Assertions;
-import org.openqa.selenium.By;
-import org.openqa.selenium.Dimension;
-import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.Assert;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
@@ -12,25 +11,25 @@ import org.testng.annotations.Test;
 
 public class FirstTest {
 
-    WebDriver driver;
 
-    @BeforeTest
-    public void driverSetup(){
-        System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver.exe");
-        driver = new ChromeDriver();
-        driver.manage().window().setSize(new Dimension(1280, 720));
-    }
-
-    @AfterTest
-    public void driverQuit() {
-        //Zamyka okno przeglądarki
-        driver.close();
-        //Zamyka sesje
-        driver.quit();
+    public WebDriver getDriver(String browser) {
+        switch (browser) {
+            case "chrome":
+                System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver.exe");
+                return new ChromeDriver();
+            case "Firefox":
+                System.setProperty("webdriver.gecko.driver", "src/main/resources/geckodriver.exe");
+                return new FirefoxDriver();
+            default:
+                throw new InvalidArgumentException("Invalid browser name!!!!");
+        }
     }
 
     @Test
     public void navigate() {
+        WebDriver driver = getDriver("Firefox");
+        driver.manage().window().maximize();
+        driver.manage().window().setSize(new Dimension(1280, 720));
         driver.get("https://pl.wikipedia.org/wiki/Wikipedia:Strona_g%C5%82%C3%B3wna");
         driver.navigate().to("https://nasa.gov");
         driver.navigate().back();
@@ -41,6 +40,22 @@ public class FirstTest {
         driver.navigate().forward();
         String titleNasa = "NASA";
         Assert.assertEquals(titleNasa,driver.getTitle(), "The title of the page is not" + titleNasa);
+        JavascriptExecutor executor = (JavascriptExecutor) driver;
+        executor.executeScript("window.open('https://www.google.com', 'blank')");
+        driver.switchTo().frame(0);
+
+
+        driver.navigate().to("https://google.com");
+        WebElement agreeButton = driver.findElement(By.xpath("/html/body/div[2]/div[2]/div[3]/span/div/div/div/div[3]/div[1]/button[2]/div"));
+        agreeButton.click();
+        WebElement searchField = driver.findElement(By.name("q"));
+        searchField.sendKeys("Selenium");
+        searchField.sendKeys(Keys.ENTER);
+        driver.quit();
+//        WebElement result = driver.findElement(By.xpath("/html/body/div[7]/div/div[10]/div[1]/div[2]/div[2]/div/div/div[1]/div/div/div/div[1]/div/a/div/cite"));
+//        Assert.assertTrue(result.isDisplayed());
+
 
     }
 }
+
